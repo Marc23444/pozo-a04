c7f486e87e9d69230e9678604ceb339ee9afc99d
Marc23444 <36685499+Marc23444@users.noreply.github.com>
2021-10-12 16:31:45 -0400

    Added all base projects

---
+++ exercise41/src/main/java/baseline/Solution41.java
@@ -0,0 +1,11 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Marcelino Pozo
+ */
+/*
+    PSUEDOCODE
+ */
+package baseline;
+
+public class Solution41 {
+}
---
+++ exercise46/src/main/java/baseline/Solution46.java
@@ -0,0 +1,5 @@
+
+package baseline;
+
+public class Solution46 {
+}
