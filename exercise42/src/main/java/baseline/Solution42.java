776ebdba63f0aa073f522a2abbe71dbd44e1df5b
Marc23444 <36685499+Marc23444@users.noreply.github.com>
2021-10-14 17:11:21 -0400

    Exercise42 Skeleton + UML Diagram. No junit needed.

--- exercise42/src/main/java/baseline/Solution42.java
+++ exercise42/src/main/java/baseline/Solution42.java
@@ -4,8 +4,32 @@
  */
 /*
     PSUEDOCODE
+    -Read in the csv file using a scanner and the File class
+        -Add each comma seperated line to an arrayList of strings
+    -Send that list of strings to a function called output
+    -Display the data in a tabular format
  */
 package baseline;
 
+import java.util.ArrayList;
+
 public class Solution42 {
+
+    //Driver function, will call input
+    public static void main(String[] args) {
+
+    }
+
+    //Will use a scanner and a File class to take in the data
+    private static void input()
+    {
+
+
+    }
+
+    //Will display the data in a tabular format
+    private static void output(ArrayList<String> list)
+    {
+
+    }
 }
