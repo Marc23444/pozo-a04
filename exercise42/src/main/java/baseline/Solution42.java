5342f85ac932270b062ce8927ab064aef4f38212
Marc23444 <36685499+Marc23444@users.noreply.github.com>
2021-10-14 17:30:30 -0400

    Exercise42 Fully Completed

--- exercise42/src/main/java/baseline/Solution42.java
+++ exercise42/src/main/java/baseline/Solution42.java
@@ -11,25 +11,74 @@
  */
 package baseline;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Solution42 {
 
     //Driver function, will call input
     public static void main(String[] args) {
-
+        input();
     }
 
     //Will use a scanner and a File class to take in the data
     private static void input()
     {
+        ArrayList<String> data = new ArrayList<>();
 
+        File input;
+        input = new File("data\\exercise42_input.txt");
+
+
+        //Try catch block to make sure the file exists
+        try{
+
+            Scanner in = new Scanner(input);
+            String temp;
+
+            while (in.hasNextLine())
+            {
+                temp = in.nextLine();
+                String[] tokens = temp.split(",");
+
+                for (int i = 0; i < tokens.length; i++)
+                {
+                    data.add(tokens[i]);
+                }
+
+            }
+
+
+            in.close();
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        output(data);
 
     }
 
     //Will display the data in a tabular format
     private static void output(ArrayList<String> list)
     {
+        int count =0;
+        System.out.println("Last      First     Salary");
+        System.out.println("--------------------------");
 
+
+        //Uses printf formatting too display the data
+        for (int i = 0; i < list.size(); i++)
+        {
+            System.out.printf("%-12s",list.get(i));
+            count++;
+            if(count % 3 == 0)
+            {
+                System.out.println();
+            }
+
+        }
     }
 }
