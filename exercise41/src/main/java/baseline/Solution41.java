326ccf3d0cd8f451de2e5527db2de998be94ac3b
Marc23444 <36685499+Marc23444@users.noreply.github.com>
2021-10-14 15:46:28 -0400

    Exercise41 Skeleton + JUnitTest + UML Diagram

--- exercise41/src/main/java/baseline/Solution41.java
+++ exercise41/src/main/java/baseline/Solution41.java
@@ -4,8 +4,42 @@
  */
 /*
     PSUEDOCODE
+    -Read in the data of the file using the scanner class
+    -This operation is done in a function called input
+    -Then that data is taken as an arrayList of strings and brought to a function called sort
+        -The data will be sorted using nested for loops, a brute force method
+    -The sorted array is then passed to an output function that will display the list in a tabular format
  */
 package baseline;
 
+import java.util.ArrayList;
+
 public class Solution41 {
+
+    //Driver class, will call input
+    public static void main(String[] args) {
+
+    }
+
+    //Takes in input from a file and sends that data too sort
+    private static void input()
+    {
+
+
+    }
+
+    //Sorts the data using a nested for loop and sends the sorted data to output
+    //THIS IS ONLY PUBLIC IN THE FIRST COMMIT FOR TESTING PURPOSES
+    public static ArrayList<String> sort(ArrayList<String> list)
+    {
+
+    }
+
+    //Displays the data in a tabular format
+    private static void output(ArrayList<String> sortedList)
+    {
+
+    }
+
+
 }
---
+++ exercise41/src/main/java/baseline/Solution41Test.java
@@ -0,0 +1,12 @@
+package baseline;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution41Test {
+
+    @Test
+    void sort() {
+    }
+}
