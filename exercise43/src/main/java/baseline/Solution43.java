7317409fc65b76d604ef7fc8d012e40980b38cbb
Marc23444 <36685499+Marc23444@users.noreply.github.com>
2021-10-15 12:46:45 -0400

    Exercise43 Skeleton + UML Diagram + Junit test skeleton added

--- exercise43/src/main/java/baseline/Solution43.java
+++ exercise43/src/main/java/baseline/Solution43.java
@@ -4,8 +4,49 @@
  */
 /*
     PSUEDOCODE
+    -First input will be prompted and taken from the user using a scanner
+        -This will happen in a function called input
+    -A main folder will then be created using the file class.
+    -If the user wants a folder for JavaScript files the function jsFolder will be called
+        -The folder will be created using the File class
+    -If the user wants a fold for CSS files the function cssFolder will be called
+        -The folder will be created using the File class
+    -Finally the function createHTMLFile will be called and the Site name and Author strings will be passed through
+        -This method will use the File and FileWriter class to create and write to the .html file
+
+
  */
 package baseline;
 
 public class Solution43 {
+
+    //Driver function, used to run input
+    public static void main(String[] args) {
+
+    }
+
+    //Takes input through a scanner, will call all other functions based on input
+    private static void input()
+    {
+
+    }
+
+    //Creates a folder inside the main folder called js
+    private static void jsFolder()
+    {
+
+    }
+
+    //Creates a folder inside the main folder called css
+    private static void cssFolder()
+    {
+
+    }
+
+    //Creates and writes to the index.html file and places it in the main folder
+    public static void createHTMLFile(String siteName, String author)
+    {
+
+    }
+
 }
---
+++ exercise43/src/main/java/baseline/Solution43Test.java
@@ -0,0 +1,11 @@
+package baseline;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution43Test {
+
+    @org.junit.jupiter.api.Test
+    void main() {
+
+    }
+}
