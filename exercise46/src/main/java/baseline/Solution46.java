2cccc4b2c83c30e49557fc1360528efa4642a41f
Marc23444 <36685499+Marc23444@users.noreply.github.com>
2021-10-16 21:55:58 -0400

    Exercise46 Skeleton + UML Diagram + Junit Test

--- exercise46/src/main/java/baseline/Solution46.java
+++ exercise46/src/main/java/baseline/Solution46.java
@@ -1,5 +1,43 @@
-
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Marcelino Pozo
+ */
+/*
+    PSUEDOCODE
+    -First we have a function called fileIn that takes in the file as a string using a scanner
+        -This string is then split at each space to create a list of words
+    -This list of words is then sent to a function called checkWords which will keep track of the
+    total number of each word
+    -This data is then sent to a function called output
+        -The data is then displayed as a histogram
+ */
 package baseline;
 
 public class Solution46 {
+
+    //Driver function runs fileIn
+    public static void main(String[] args) {
+
+    }
+
+    private static void fileIn()
+    {
+
+    }
+
+    //This will check each word and compare it to every other word to find if its unique or not
+    //Then it will keep track of the amount that word occurs
+    //(Dont really know how im gonna do this yet)
+    private static void checkWords(String[] words)
+    {
+
+    }
+
+    //Will display data as a histogram, havent figured out how im gonna store the data so theres no param as of now
+    private static void output()
+    {
+
+    }
+
+
 }
