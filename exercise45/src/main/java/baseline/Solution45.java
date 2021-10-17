7ca3252b86df403cfe02ffb52ee51167ead8e131
Marc23444 <36685499+Marc23444@users.noreply.github.com>
2021-10-16 21:39:30 -0400

    Exercise45 Skeleton + UML Diagram + Junit

--- exercise45/src/main/java/baseline/Solution45.java
+++ exercise45/src/main/java/baseline/Solution45.java
@@ -1,4 +1,46 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Marcelino Pozo
+ */
+/*
+    PSUEDOCODE
+    -Create a function called fileIn that takes in the file a single string
+        -The user will enter the name of the file
+        -A try catch block will be used to make sure the file exists
+    -Then split that string at each space character creating an array of strings
+    -Pass that string array to a function called checkString that looks for all
+    occurrences of the word utilize and replaces them with the word use
+    -Then pass that modified array to a function called fileOut
+    -Finally write this modified string array to a new file called "exercise45_output.txt"
+ */
 package baseline;
 
 public class Solution45 {
+
+
+    //Driver function, calls fileIn
+    public static void main(String[] args) {
+
+    }
+
+    //Uses the File class and a scanner to copy the data from the file
+    //It will then use regex to split the string into an array of strings
+    private static void fileIn()
+    {
+
+    }
+
+    //Checks each word in the string and looks for the string "utilize", replaces every
+    //instance of utilize with use
+    public static void checkString(String[] words)
+    {
+
+    }
+
+    //Creates a new file called "exercise45_output.txt" and writes to it
+    private static void fileOut(String[] words)
+    {
+
+    }
+
 }
---
+++ exercise45/src/main/java/baseline/Solution45Test.java
@@ -0,0 +1,10 @@
+package baseline;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution45Test {
+
+    @org.junit.jupiter.api.Test
+    void checkString() {
+    }
+}
