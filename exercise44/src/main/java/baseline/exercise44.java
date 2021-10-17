14c79935edf805ca62cecb70030605bc0094e368
Marc23444 <36685499+Marc23444@users.noreply.github.com>
2021-10-16 21:23:30 -0400

    Exercise44 Skeleton

--- exercise44/src/main/java/baseline/exercise44.java
+++ exercise44/src/main/java/baseline/exercise44.java
@@ -4,8 +4,40 @@
  */
 /*
     PSUEDOCODE
+    -Create a function called input which will take in user input
+        -This input will be taken in through a scanner
+    -Once the user enters something the function checkInventory will be called
+        -If the function returns true the user input will be passed to a function
+        called displayProduct
+        -If checkInventory returns false, the user will be prompted to enter in new input
+        this is accomplished through a while loop
+    -If displayProduct is ran it will use the gson library to parse through and display the data
  */
 package baseline;
 
 public class exercise44 {
+
+    //Driver function, calls input
+    public static void main(String[] args) {
+
+    }
+
+    //Uses a scanner to take in input inside of a while loop
+    private static void input()
+    {
+
+    }
+
+    //Uses the fromJson() to convert the json file
+    //Also uses the File class to grab the file
+    private static boolean checkInventory(String item)
+    {
+
+    }
+
+    //Displays the data
+    private static void displayProduct(String item)
+    {
+
+    }
 }
